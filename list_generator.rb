2f3aabcf83675578ca3d26535327f9bf9a35eb3a
Randy Coburn <morfien101@gmail.com>
2017-03-12 12:59:38 +0000

    improved the recusion method. Added a bonus lookup for startup.

--- list_generator.rb
+++ list_generator.rb
@@ -1,29 +1,31 @@
 RUNES = (%w(a b c d e f) + [2,3,4,5,6,7,8,9]).shuffle
 FILE_NAME = ARGV[0]
 
-guess = [""] * 10
+guess = RUNES[0..9]
 
 def generator(cur_state, index)
-  if index < 9
-    RUNES.each do |r|
-      cur_state[index] = r
-      if cur_state[index] == cur_state[index -1 ] &&
-         cur_state[index] == cur_state[index -2 ]
-         next
-      end if index >= 2
-      generator(cur_state, index + 1)
-    end
-  else
-    open(FILE_NAME, 'w') do |file|
-      RUNES.each do |r|
+  RUNES.each do |r|
+    cur_state[index] = r
+    # Chances are that we will never get more than 2 of the same letter in a row
+    if cur_state[index] == cur_state[index -1 ] &&
+       cur_state[index] == cur_state[index -2 ]
+       next
+    end if index > 1
+    generator(cur_state, index + 1) if index < 9
+    open(FILE_NAME, 'a') do |file|
         cur_state[index] = r
         file.puts cur_state.join("")
-      end
     end
   end
 end
 
-File.new(FILE_NAME,'w').close
+def create_file(guess)
+  File.new(FILE_NAME,'w').close
+  open(FILE_NAME, 'a') do |file|
+      file.puts guess.join()
+  end
+end
 
 puts "Starting at #{RUNES.to_s}"
+create_file(guess)
 generator(guess, 0)
