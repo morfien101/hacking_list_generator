8e17c64f078dde36ac6e1fa8e96dcf99f5e72ede
Randy Coburn <morfien101@gmail.com>
2017-03-12 13:47:33 +0000

    updated the script to have a help menu and to be more flexable.

--- list_generator.rb
+++ list_generator.rb
@@ -1,7 +1,27 @@
-RUNES = (%w(a b c d e f) + [2,3,4,5,6,7,8,9]).shuffle
-FILE_NAME = ARGV[0]
+#!/bin/env ruby
+# This script was to be used to create a list of passwords for
+# a BT Homehub5 which is then passed onto something like airocrack.
+# This script is for learning purposed only.
+# Use at your own risk.
+# Licence: Apache 2.0
 
-guess = RUNES[0..9]
+# READ THIS HELP SECTION FOR HOW TO USE THIS SCRIPT.
+# Very simple help menu
+if ARGV[0] =~ /-h|--help/
+  puts 'You can to pass in a file to hold the keys as arugment 1 or you will get a'
+  puts 'keys.txt file in the root of the directory you are running this script from.'
+  puts 'you can also pass in a ruleset for the list as arguments 2 and 3 for'
+  puts 'charactor array and password length.'
+  puts 'Example:'
+  puts ' ruby list_generator.rb /tmp/keys.txt "%w(a b c d e f) + [2,3,4,5,6,7,8,9]" 10'
+  exit 0
+end
+
+FILE_NAME = ARGV[0].nil? ? 'keys.txt' : ARGV[0]
+RUNES = ARGV[1].nil? ? (%w(a b c d e f) + [2,3,4,5,6,7,8,9]).shuffle : eval(ARGV[1].shuffle)
+LENGTH = ARGV[2].nil? ? 10 : ARGV[2].to_i - 1
+
+guess = RUNES[0..LENGTH]
 
 def generator(cur_state, index)
   RUNES.each do |r|
@@ -11,10 +31,10 @@ def generator(cur_state, index)
        cur_state[index] == cur_state[index -2 ]
        next
     end if index > 1
-    generator(cur_state, index + 1) if index < 9
+    generator(cur_state, index + 1) if index < LENGTH - 1
     open(FILE_NAME, 'a') do |file|
         cur_state[index] = r
-        file.puts cur_state.join("")
+        file.puts cur_state.join
     end
   end
 end
